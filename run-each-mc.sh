21d60f9d1812499241f70256b43c49585b244c2a
Siphalor <info@siphalor.de>
2025-11-22 12:07:20 +0100

    feat: Colorize output

--- run-each-mc.sh
+++ run-each-mc.sh
@@ -1,5 +1,10 @@
 #! /bin/env sh
 
+FMT_RED="\033[31m"
+FMT_GREEN="\033[32m"
+FMT_BLUE="\033[34m"
+FMT_CLEAR="\033[0m"
+
 if [ $# -eq 0 ] ; then
   echo "Error: must supply the command to run for every MC version"
   exit 1
@@ -10,17 +15,21 @@ echo "Discovered MC versions:
 $mcVersions"
 
 for mcVersion in $mcVersions ; do
-  echo "Running \"$*\" for MC version $mcVersion"
+  printf '%bRunning "%s" for MC version %s%b\n\n' "$FMT_BLUE" "$*" "$mcVersion" "$FMT_CLEAR"
   sed gradle.properties -i -e "s#\(minecraft\.version\.descriptor = \).*#\1$mcVersion#"
   if "$@" ; then
     succeeded="$mcVersion $succeeded"
   else
     failed="$mcVersion $failed"
   fi
+  echo
 done
 
-echo "Succeeded: ${succeeded-none}"
-echo "Failed: ${failed-none}"
+printf '%bResults:%b\n' "$FMT_BLUE" "$FMT_CLEAR"
+if [ -n "$succeeded" ] ; then
+  printf '%bSucceeded: %s%b\n' "$FMT_GREEN" "$succeeded" "$FMT_CLEAR"
+fi
 if [ -n "$failed" ] ; then
+  printf '%bFailed: %s%b\n' "$FMT_RED" "$failed" "$FMT_CLEAR"
   exit 1
 fi
